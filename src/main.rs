09bf60fa5c0d9f399731c9143e8df933716d729e
梶塚太智 <kajizukataichi@outlook.jp>
2024-09-28 05:12:28 +0900

    Implement function `type`

--- src/main.rs
+++ src/main.rs
@@ -82,6 +82,10 @@ fn main() {
                 _ => Some(params[0].clone()),
             }),
         ),
+        (
+            "type".to_string(),
+            Type::Function(|params| Some(Type::String(params[0].get_type()))),
+        ),
         ("exit".to_string(), Type::Function(|_| exit(0))),
     ]);
 
