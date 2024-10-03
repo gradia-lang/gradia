bc24e6b191e91b70bb005d52c97b9c1dff99e06b
梶塚太智 <kajizukataichi@outlook.jp>
2024-10-04 05:46:57 +0900

    Add structure

--- gradia-wasm/src/lib.rs
+++ gradia-wasm/src/lib.rs
@@ -1,33 +1,67 @@
 use gradia_core::{builtin_function, parse, tokenize, Function, Type};
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn run(code: String) -> String {
-    let scope = &mut builtin_function();
-    scope.insert("stdout".to_string(), Type::String(String::new()));
-    scope.insert(
-        "print".to_string(),
-        Type::Function(Function::BuiltIn(|params, scope| {
-            scope.insert(
-                "stdout".to_string(),
-                Type::String(
-                    scope.get("stdout")?.get_string()
-                        + &params
-                            .iter()
-                            .map(|i| i.get_string())
-                            .collect::<Vec<String>>()
-                            .concat(),
-                ),
-            )
-        })),
-    );
+pub struct Gradia {
+    scope: HashMap<String, Type>,
+}
 
-    if let Some(lines) = tokenize(code) {
-        for line in lines {
-            if let Some(ast) = parse(line) {
-                ast.eval(scope);
+#[wasm_bindgen]
+impl Gradia {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Gradia {
+        let mut scope = builtin_function();
+        scope.insert("stdout".to_string(), Type::String(String::new()));
+        scope.insert(
+            "print".to_string(),
+            Type::Function(Function::BuiltIn(|params, scope| {
+                scope.insert(
+                    "stdout".to_string(),
+                    Type::String(
+                        scope.get("stdout")?.get_string()
+                            + &params
+                                .iter()
+                                .map(|i| i.get_string())
+                                .collect::<Vec<String>>()
+                                .concat(),
+                    ),
+                )
+            })),
+        );
+        Gradia { scope }
+    }
+
+    pub fn run(&mut self, code: String) {
+        if let Some(lines) = tokenize(code) {
+            for line in lines {
+                if let Some(ast) = parse(line) {
+                    ast.eval(&mut self.scope);
+                }
             }
         }
     }
-    scope.get("stdout").unwrap().get_string()
+
+    pub fn eval(&mut self, code: String) -> String {
+        let mut result = String::new();
+        if let Some(lines) = tokenize(code) {
+            for line in lines {
+                if let Some(ast) = parse(line) {
+                    result = format!("{:?}", ast.eval(&mut self.scope));
+                }
+            }
+        }
+        result
+    }
+
+    pub fn get_stdout(&self) -> String {
+        self.scope.get("stdout").unwrap().get_string()
+    }
+}
+
+#[wasm_bindgen]
+pub fn run_gradia(code: String) -> String {
+    let mut gradia = Gradia::new();
+    gradia.run(code);
+    gradia.get_stdout()
 }
